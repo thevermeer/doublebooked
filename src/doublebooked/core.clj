b1276868b86911bdc8eb26c5b50c7e400aca0993
Jeff Vermeer <jeff.vermeer@gmail.com>
2018-02-26 22:42:29 -0500

    minor update

--- src/doublebooked/core.clj
+++ src/doublebooked/core.clj
@@ -2,13 +2,19 @@
   (:require [clj-time.format :as tf]
             [clj-time.core :as t]))
 
+;; ---------------------------------------------------
 ;; ---- CLJ Solution to the double-booked problem ----
+;; ---------------------------------------------------
 ;;
 ;; This solution accepts a collection of maps, each with a :start and :end key
 ;; Both :start and :end are string representations of date/times
 ;; eg. {:start "2018-02-26T14:00:00.000Z" :end "2018-02-26T18:00:00.000Z"}
 ;;
 ;; Sample usage: (get-overlapping-events events)
+
+;; -------------------------------------------------
+;; ---- String->Date Coersion ----
+;; -------------------------------------------------
 
 (defn ->date
   "Converts a string to a clj(s) date
@@ -19,7 +25,7 @@
     (catch Exception e
     nil)))
 
-(defn str-before
+(defn str-before?
   "Accepts two string representations as dates.
    Returns boolean of da 'before or equal' db"
   [da db]
@@ -27,15 +33,21 @@
         d2 (->date db)]
     (t/before? d1 d2)))
 
+;; -------------------------------------------------    
+;; ---- Recursive Function ----   
+;; -------------------------------------------------
+
 (defn recur-compare
   "Recursively compares a list of events sorted by start time
-   Returns a list of sets, with each set containing the pair of
+   Compares the end time of head of the list with the remaining events 
+   (ie. events starting after the head)
+   Returns a list of sets, with each set containing the pair of 
    overlapping events"
   [{e1e :end :as ev} coll]
   ;; Filter + Transform the overlaps into #{a b}
   (let [res (keep
               (fn [{e2s :start :as ev2}]
-                (when (str-before e2s e1e)
+                (when (str-before? e2s e1e)
                   #{ev ev2}))
               coll)
         rem  (rest coll)]
@@ -45,16 +57,22 @@
               ;; recursive step
               (recur-compare (first coll) rem)))))
 
-(defn get-overlapping-events
- "Receives a collection of maps with keys
+;; -------------------------------------------------
+;; ---- Solution Function ----
+;; -------------------------------------------------
+
+(defn get-overlapping-events  
+ "Receives a collection of maps with keys:
   :start, :end - both expecting string representations of date/time
   in the form '2018-02-26T12:00:00.000Z'
   Returns a set of sets, with each set containing the pair of
   overlapping events"
  [coll]
  (if (> (count coll) 1)
-   (let [tcoll (vec (sort-by :start coll))]
-     (into #{} (recur-compare (first tcoll) (rest tcoll))))
+   ;; Step 1 :: Sort the collection by :start
+   (let [s-coll (vec (sort-by :start coll))]
+     ;; Step 2 :: recur through the sorted list
+     (into #{} (recur-compare (first s-coll) (rest s-coll))))
    #{}))
 
 
